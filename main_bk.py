0383000012404bc6741f0b24da88d4c9f4e7f8d0
masu <yasumoto.7991mm@gmail.com>
2023-07-06 10:30:15 +0700

    create bk

---
+++ main_bk.py
@@ -0,0 +1,31 @@
+# discord.pyをインポート
+import discord
+
+# .envファイルからTOKENの値を読み込み
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# 初期化
+client = discord.Client(intents=discord.Intents.all())
+
+# Botが正常に起動した際のイベント設定
+@client.event
+async def on_ready():
+    print('Startup Success!!!')
+
+# Botがメッセージを読み込んだ際のイベント設定
+@client.event
+async def on_message(message):
+    # Botが出力したメッセージじゃ無い場合（この条件でBotのメッセージを弾く）
+    if not message.author.bot:
+        # 入力されたメッセージが「はじめまして」ならBotが「よろしくおねがいします」と出力する設定
+        if message.content == 'はじめまして':
+            await message.channel.send('よろしくおねがいします')
+
+    # 上記と同様に2個目の設定
+    if message.content == 'さようなら':
+        await message.channel.send('またあいましょう')
+
+# TOKENの値を読み込み、Botを起動させる
+client.run(os.getenv('TOKEN'))
