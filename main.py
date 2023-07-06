38dea34042fbe62bc39118475a5470eef860a8a1
masu <yasumoto.7991mm@gmail.com>
2023-07-06 11:31:00 +0700

    create is_int method

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ client = discord.Client(intents=discord.Intents.all())
 # 合計金額を保持するための変数を0で初期化
 sum_money = 0
 
+#引数が整数か判定する関数
+def is_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 # Botが正常に起動した際のイベント設定
 @client.event
 async def on_ready():
@@ -22,8 +30,8 @@ async def on_ready():
 async def on_message(message):
     # Botが出力したメッセージじゃ無い場合（この条件でBotのメッセージを弾く）
     if not message.author.bot:
-        # 入力されたメッセージが数字の場合
-        if message.content.isdecimal():
+        # 入力されたメッセージが整数の場合
+        if is_int(message.content):
             global sum_money  # この行でグローバル変数を使用することを明示的に宣言します。
             sum_money += int(message.content)
             await message.channel.send(sum_money)  # 'sum' ではなく 'sum_money' を使用します
